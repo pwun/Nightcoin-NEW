e3a9cc7029526277d8694a97fcc003a5036855c4
nightcoin <m.oliva@nightcoin.de>
2015-04-12 19:46:17 +0200

    Coin Layout

--- app/build/generated/source/r/debug/de/nightcoin/R.java
+++ app/build/generated/source/r/debug/de/nightcoin/R.java
@@ -1309,14 +1309,18 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int black_coin=0x7f020033;
-        public static final int coin=0x7f020034;
-        public static final int ic_action_important=0x7f020035;
-        public static final int ic_action_not_important=0x7f020036;
-        public static final int ic_action_settings=0x7f020037;
-        public static final int ic_launcher=0x7f020038;
-        public static final int logo=0x7f020039;
-        public static final int white_arrow=0x7f02003a;
+        public static final int bars_thumbnail=0x7f020033;
+        public static final int black_coin=0x7f020034;
+        public static final int clubs_thumbnail=0x7f020035;
+        public static final int coin=0x7f020036;
+        public static final int favorites_thumbnail=0x7f020037;
+        public static final int ic_action_important=0x7f020038;
+        public static final int ic_action_not_important=0x7f020039;
+        public static final int ic_action_settings=0x7f02003a;
+        public static final int ic_launcher=0x7f02003b;
+        public static final int logo=0x7f02003c;
+        public static final int nightcoins_main_act_button_bg=0x7f02003d;
+        public static final int white_arrow=0x7f02003e;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080097;
