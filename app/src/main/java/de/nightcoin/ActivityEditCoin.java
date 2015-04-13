668e6326e37829f5d3a704d8bd5e5d8c14b9de2a
nightcoin <m.oliva@nightcoin.de>
2015-04-13 22:26:40 +0200

    jhk

--- app/src/main/java/de/nightcoin/ActivityEditCoin.java
+++ app/src/main/java/de/nightcoin/ActivityEditCoin.java
@@ -1,26 +1,32 @@
 package de.nightcoin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.DeleteCallback;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
-import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
 public class ActivityEditCoin extends ActionBarActivity {
 
     ParseObject serverObject;
-    EditText editDate;
+    DatePicker datePicker;
     EditText editValue;
     EditText editAmount;
     Button saveButton;
@@ -37,13 +43,35 @@ public class ActivityEditCoin extends ActionBarActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Date coinDate = new Date();
+                coinDate.setYear(datePicker.getYear());
+                coinDate.setMonth(datePicker.getMonth());
+                coinDate.setDate(datePicker.getDayOfMonth());
+                coinDate.setHours(21);
+                serverObject.put("date", coinDate);
+                serverObject.put("value", editValue.getText().toString());
+                serverObject.put("amount", Integer.parseInt(editAmount.getText().toString()));
+                serverObject.put("limited", true);
+                serverObject.put("city", "Regensburg");
+                serverObject.put("location", ParseUser.getCurrentUser().get("location").toString());
+                serverObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast t = Toast.makeText(ActivityEditCoin.this, "Coin wurde erfolgreich gespeichert.", Toast.LENGTH_SHORT);
+                            t.show();
+                        } else {
+                            Toast t = Toast.makeText(ActivityEditCoin.this, "Fehler: Coin konnte nicht gespeichert werden.", Toast.LENGTH_SHORT);
+                            t.show();
+                        }
+                    }
+                });
             }
         });
     }
 
     private void init(){
-        editDate = (EditText) findViewById(R.id.editTextActivityEditCoinsDate);
+        datePicker = (DatePicker) findViewById(R.id.datePickerActivityEditCoins);
         editValue = (EditText) findViewById(R.id.editTextActivityEditCoinsValue);
         editAmount = (EditText) findViewById(R.id.editTextActivityEditCoinsAmount);
         saveButton = (Button) findViewById(R.id.buttonActivityEditCoinsSave);
@@ -58,7 +86,8 @@ public class ActivityEditCoin extends ActionBarActivity {
             public void done(List<ParseObject> objects, ParseException e) {
                 // TODO Auto-generated method stub
                 serverObject = objects.get(0);
-                editDate.setText(new SimpleDateFormat("dd. MMMM yyyy").format(serverObject.getDate("date")));
+                Date objectDate = (serverObject.getDate("date"));
+                datePicker.init(objectDate.getYear(), objectDate.getMonth(), objectDate.getDay(), null);
                 editValue.setText(serverObject.get("value").toString());
                 editAmount.setText(serverObject.get("amount").toString());
             }
@@ -74,6 +103,12 @@ public class ActivityEditCoin extends ActionBarActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent i = new Intent(ActivityEditCoin.this, ActivityStandardList.class);
+        ActivityEditCoin.this.startActivity(i);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -81,8 +116,21 @@ public class ActivityEditCoin extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_delete) {
+            serverObject.deleteInBackground(new DeleteCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Intent i = new Intent(ActivityEditCoin.this, ActivityStandardList.class);
+                        ActivityEditCoin.this.startActivity(i);
+                        Toast t = Toast.makeText(ActivityEditCoin.this, "Coin wurde erfolgreich gelöscht.", Toast.LENGTH_SHORT);
+                        t.show();
+                    } else {
+                        Toast t = Toast.makeText(ActivityEditCoin.this, "Fehler: Coin konnte nicht gelöscht werden.", Toast.LENGTH_SHORT);
+                        t.show();
+                    }
+                }
+            });
         }
 
         return super.onOptionsItemSelected(item);
