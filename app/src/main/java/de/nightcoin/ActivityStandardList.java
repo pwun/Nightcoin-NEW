3c9117b902e3031f85960cd73bcb407b34fdbad0
pwun <wunderlich.mobil@gmail.com>
2015-04-17 19:02:51 +0200

    mehr laden...

--- app/src/main/java/de/nightcoin/ActivityStandardList.java
+++ app/src/main/java/de/nightcoin/ActivityStandardList.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 import com.parse.DeleteCallback;
@@ -59,6 +60,9 @@ public class ActivityStandardList extends ActionBarActivity {
             }
             @Override
             public void onLoaded(final List<ParseObject> objects, Exception e) {
+                if(objects.size()==0){
+                    findViewById(R.id.textViewListNoData).setVisibility(View.VISIBLE);
+                }
                 progressDialog.dismiss();
                 ParseObject.unpinAllInBackground(contentMode, new DeleteCallback() {
 
